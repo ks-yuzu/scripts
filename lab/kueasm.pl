5bcca1acf16b68711003b60c781c891c40b198a6
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-07-05 00:11:29 +0900

    Update lab/kuesim.pl

--- lab/kueasm.pl
+++ lab/kueasm.pl
@@ -9,8 +9,8 @@ use open IO => qw/:encoding(UTF-8) :std/;
 my $asm_name = shift;
 die q/suffix is not '.asm'/ unless $asm_name =~ /\.asm$/;
 
-system qq!perl ~/works/lab/astem/kuechip/assembler/assembler.pl $asm_name!;
+system qq!perl ~/works/lab/astem/kuesim/assembler/assembler.pl $asm_name!;
 
 my $bin_name = $asm_name =~ s/\.[^\.]*/.bin/r;
 
-system qq!/bin/cat ~/works/lab/astem/kuechip/simulator/template-head.html $bin_name ~/works/lab/astem/kuechip/simulator/template-tail.html > ~/works/lab/astem/kuechip/simulator/kuesim.html!;
+system qq!/bin/cat ~/works/lab/astem/kuesim/simulator/template-head.html $bin_name ~/works/lab/astem/kuesim/simulator/template-tail.html > ~/works/lab/astem/kuesim/simulator/kuesim.html!;
