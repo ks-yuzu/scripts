d48b99ddaf3e90654f3660bbf3818a7a7c3ddb7c
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-07-24 14:55:41 +0900

    Update archive.pl

--- lab/archive.pl
+++ lab/archive.pl
@@ -5,57 +5,66 @@ use warnings;
 use HTTP::Request::Common;
 use LWP::UserAgent;
 use Getopt::Kingpin;
+use List::Util qw/max/;
+use DDP;
 
 use lib "$ENV{HOME}/works/";
 use MyPassword::Archive;                          # ID, PW
 
 # constant
-my $url = 'http://ist.ksc.kwansei.ac.jp/~ishiura/cgi-bin/webfiler/lab/main.cgi';
-my $user = MyPassword::Archive::USER;
-my $pass = MyPassword::Archive::PASS;
+my $URL = 'http://ist.ksc.kwansei.ac.jp/~ishiura/cgi-bin/webfiler/lab/main.cgi';
+my $USER = MyPassword::Archive::USER;
+my $PASS = MyPassword::Archive::PASS;
 
 sub { # main
   my $kingpin = Getopt::Kingpin->new();
-  my $f_list    = $kingpin->flag('list',    'show the list of files')->short('l')->bool;
-  my $f_upload  = $kingpin->flag('upload',  'upload files'          )->short('u')->bool;
-  my $f_verbose = $kingpin->flag('verbose', 'show http response'    )->short('v')->bool;
+  my %options;
+  $options{f_show}    = $kingpin->flag('show',    'show the list of files')->short('s')->bool;
+  $options{f_upload}  = $kingpin->flag('upload',  'upload files'          )->short('u')->bool;
+  $options{f_list}    = $kingpin->flag('long',    'long listing (like ls)')->short('l')->bool;
+  $options{f_verbose} = $kingpin->flag('verbose', 'show http response'    )->short('v')->bool;
   my $args_obj  = $kingpin->arg('args',     'dir file1 file2 ...'   )->string_list;
   $kingpin->parse;
 
   my @args = @{ $args_obj->value };
 
   # list, upload 指定がなければ list
-  if ( ! $f_list and ! $f_upload ) {
-    $f_list = 1;
+  if ( ! $options{f_show} and ! $options{f_upload} ) {
+    $options{f_show} = 1;
   }
 
   # パスワードファイルチェック
-  die "[error] no loggin information\n" if ! defined $user || ! defined $pass;
+  die "[error] no loggin information\n" if ! defined $USER || ! defined $PASS;
 
   # ファイル一覧表示
-  if ( $f_list ) {
+  if ( $options{f_show} ) {
     my $dir = shift @args // die "specify the upload dir"; # ディレクトリチェック
-    list($dir, $f_verbose);
+    list($dir, \%options);
   }
 
   # アップロード
-  if ( $f_upload ) {
+  if ( $options{f_upload} ) {
     my $dir = shift @args // die "specify the upload dir"; # ディレクトリチェック
     die "no input file\n" if scalar @args == 0;            # ファイル指定チェック
     for my $file ( @args ) {                               # ファイル存在チェック
       die "file '$file' does not exist.\n" if not -f $file;
     }
-    upload($dir, \@args);
+    upload($dir, \@args, \%options);
+  }
+
+  # ダウンロード
+  if ( 1 ) {
+    
   }
 }->();
 
 
 sub list {
-  my ($dir, $f_verbose) = @_;
+  my ($dir, $options) = @_;
 
   # HTTP リクエストを作って投げる
-  my $req = HTTP::Request->new(GET => "${url}?D=archives&dir=${dir}&sortby=filename");
-  $req->authorization_basic($user, $pass);
+  my $req = HTTP::Request->new(GET => "${URL}?D=archives&dir=${dir}&sortby=filename");
+  $req->authorization_basic($USER, $PASS);
   my $ua = LWP::UserAgent->new();
   my $res = $ua->request($req);
 
@@ -64,22 +73,57 @@ sub list {
   die $res->content if $res->content =~ /Can't connect to/;
 
   # HTTP のレスポンスを表示 (-v)
-  say $res->content if $f_verbose;
+  say $res->content if $options->{f_verbose};
 
   # ディレクトリエラーチェック
   die "Invalid dir: $dir\n" if $res->content =~ /invalid dir\($dir\)/;
 
   # リスト出力
-  my @files = $res->content =~ /<input type="checkbox" name="SEL_(.*?)">/g;
-  say STDERR "files in archive/$dir";
-  # (scalar @files > 0) ? say join "\n", (map { "- $_"  } @files)
-  (scalar @files > 0) ? say join "\n", (map { "$_"  } @files)
-                      : say '(no file)';
+  list_parse_res($res->content, $options);
 };
 
 
+sub list_parse_res {
+  my ($res, $options) = @_;
+
+  # HTTP レスポンスのパース
+  my @files;
+  while ( $res =~ m|<tr>(?<item>.*?)</tr>|g ) {
+    my ($select, $actions, $filename, $size, $time) = ($+{item} =~ m|<td.*?>(?<column>.*?)</td>|g);
+
+    next if $select !~ /name="SEL_/;    # 上の階層に戻る (↑) は除外
+
+    # 余計なタグ・文字実体参照を削除
+    $filename =~ s/<.*?>//g;
+    $filename =~ s/&.*?;\s*//g;
+    $size =~ s/<.*?>//g;
+    $time =~ s/<.*?>//g;
+
+    push @files, { filename => $filename, size => $size, time => $time, };
+  }
+
+  if ( @files == 0 ) {
+    say STDERR '(no file)';
+    return;
+  }
+
+  # say STDERR "files in archive/$dir";
+
+  # ファイルリスト表示
+  if ( $options->{f_list} ) {
+    my $width_size_column = max map {length $_->{size}} @files;
+    for my $file ( @files ) {
+      printf "%s  %${width_size_column}s  %s\n", $file->{time}, $file->{size}, $file->{filename};
+    }
+  }
+  else {
+    say join "\n", map { $_->{filename} } @files;
+  }
+}
+
+
 sub upload {
-  my ($dir, $files) = @_;
+  my ($dir, $files, $options) = @_;
 
   # POST メソッドで送る content
   my $content = [
@@ -98,11 +142,12 @@ sub upload {
   }
 
   # HTTP リクエストを作って投げる
-  my $req = POST($url,
-                 Content_Type => 'form-data',
-                 Content      => $content
-            );
-  $req->authorization_basic($user, $pass);
+  my $req = POST(
+    $URL,
+    Content_Type => 'form-data',
+    Content      => $content
+  );
+  $req->authorization_basic($USER, $PASS);
   my $ua = LWP::UserAgent->new();
   my $res = $ua->request($req);
 
@@ -110,13 +155,14 @@ sub upload {
   die "no response from server\n" if not defined $res;
   die $res->content if $res->content =~ /Can't connect to/;
 
+  # HTTP のレスポンスを表示 (-v)
+  say $res->content if $options->{f_verbose};
+
   # ディレクトリエラーチェック
   die "Invalid dir: $dir\n" if $res->content =~ /invalid dir\($dir\)/;
 
-    # リスト出力
+  # リスト出力
   my @files = $res->content =~ /uploaded '(.*?)'/g;
   say "uploaded the files to archive/$dir";
   say join "\n", (map { "- $_"  } @files);
-
-  # say $res->content;
 };
